d8781139d1c83c883dbf004fb7d5bf5951123750
Adriano Luzi <adriano.luzi16@gmail.com>
2023-12-09 21:19:52 -0300

    adding firebase

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,40 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import ItemList from './ItemList';
 import { useParams } from 'react-router-dom';
+import { collection, getDocs } from 'firebase/firestore';
+import {dataBase} from '../firebase/config';
 
 const ItemListContainer = () => {
-  const { category } = useParams();
 
-  const productos = [
-    {id: 1 , name: "Cafe de Colombia", description: "Descripcion del producto ", stock: 1, category:  "variedades de cafe", price: 2600},
-    {id: 2 , name: "Cafetera Italiana", description: "Descripcion del producto ", stock: 15, category: "cafeteras", price: 2600}, 
-    {id: 3 , name: "Coladores", description: "Descripcion del producto ", stock: 20, category: "accesorios de barista", price: 2600},
-    {id: 4 , name: "Te Earl Grey", description: "Descripcion del producto ", stock: 25, category: "infusiones", price: 2600},
-    {id: 5 , name: "Delantal", description: "Descripcion del producto ", stock: 25, category: "accesorios de barista", price: 2600},
-    {id: 6 , name: "Cafetera Francesa", description: "Descripcion del producto ", stock: 25, category: "cafeteras", price: 2600},
-    {id: 7 , name: "Te English Breakfast", description: "Descripcion del producto ", stock: 25, category: "infusiones", price: 2600},
-    {id: 8 , name: "Cafe de Nicaragüa", description: "Descripcion del producto ", stock: 10, category:  "variedades de cafe", price: 2600},
-  ]
+  const [products, setProducts] = useState([]);
 
-  const getProductos = new Promise((resolve, reject) => {
-    if (productos.length > 0) {
-      setTimeout(() => {
-        resolve(productos);
-      }, 2000)
-    } else {
-      reject(new Error("No hay datos"))
-    }
-  })
+  const [name, setName] = useState("Products")
+
+  const category = useParams().category
+
+  useEffect(() => {
+
+    const prodRef = collection (dataBase, "infusiones");
+
+    getDocs(prodRef)
+      .then((resp)=> {
+        console.log(resp.docs[0].id);
+        console.log(resp.docs[0].data());
+        setProducts(
+          resp.docs.map((doc)=>{
+            return{...doc.data(), id: doc.id}
+          })
+        )
+      })
+
+  }, [category])
   
-  getProductos
-    .then((res)=>{
-      console.log(res)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-  
-  const filteredProducts = productos.filter((producto)=>producto.category === category)
+  const filteredProducts = products.filter((products)=>products.category === category)
   console.log(filteredProducts)
    
 
---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnAJPQWsLNkzkdp93j1-qWv-NesHbTLz8",
+  authDomain: "cafe-luzi.firebaseapp.com",
+  projectId: "cafe-luzi",
+  storageBucket: "cafe-luzi.appspot.com",
+  messagingSenderId: "1001691991777",
+  appId: "1:1001691991777:web:3b33f7c32655e517d3f1a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const dataBase = getFirestore(app);
